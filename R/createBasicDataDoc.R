cf01188e601a5b63e5e3e3c2ac5d6b761529f301
stephdesilva <steph@rex-analytics.com>
2018-04-15 10:19:51 +1000

    Update functions.

--- R/createBasicDataDoc.R
+++ R/createBasicDataDoc.R
@@ -1,4 +1,31 @@
 createBasicDataDoc <- function(consult_path, client, project){
   data_path <- paste(consult_path, client, project_name, "data", sep = "/")
+  # find all the .csvs
+  csv_files <- list.files(data_path, pattern = "\\w+.csv")
+  for (i in 1:length(csv_files)){
+    basicDoc(data_path, csv_files[i], "csv")
+  }
 
+}
+
+## TODO need to work out how and what format to render this file in.
+## May need to create it as a data frame object, return back to working directory
+## then render it as human readable later.
+## perhaps download the relevant file from GH to use as a template.
+
+basicDoc <- function(data_path, file, type){
+  if (type == "csv"){
+    file_name <- paste(data_path, file, sep = "/")
+    f_name <- strsplit(file, ".")
+    f_name <- f_name[1]
+    lib_location <- .libPaths()
+    mkdown_location <- paste(lib_location[1], "consultthat/man",
+                             "basicDoc.Rmd",sep = "/")
+    rmarkdown::render(mkdown_location, params = list(filename = file_name,
+                                                     type = "csv"),
+           output_file = paste(data_path, "documentation/",
+                                "Basic Documentation_", f_name, ".pdf"))
+  } else {
+    stop("Sorry, haven't implemented that file type yet.")
+  }
 }
---
+++ R/scratchpad.R
@@ -0,0 +1,7 @@
+# useful sources
+# https://rviews.rstudio.com/2017/11/07/automating-summary-of-surveys-with-rmarkdown/
+
+#   https://rmarkdown.rstudio.com/developer_parameterized_reports.html
+
+# https://github.com/r-lib/usethis
+
