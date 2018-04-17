8081b99ffa80142af861c3c03c2dd0e19504bdfe
stephdesilva <steph@rex-analytics.com>
2018-04-17 20:56:49 +1000

    Change location of basic documentation file. Change method of retrieving it to download.

--- R/createBasicDataDoc.R
+++ R/createBasicDataDoc.R
@@ -14,13 +14,13 @@ createBasicDataDoc <- function(consult_path, client, project){
 ## perhaps download the relevant file from GH to use as a template.
 
 basicDoc <- function(data_path, file, type){
+  require(RCurl)
   if (type == "csv"){
     file_name <- paste(data_path, file, sep = "/")
     f_name <- strsplit(file, ".")
     f_name <- f_name[1]
     lib_location <- .libPaths()
-    mkdown_location <- paste(lib_location[1], "consultthat/man",
-                             "basicDoc.Rmd",sep = "/")
+    mkdown_location <-
     rmarkdown::render(mkdown_location, params = list(filename = file_name,
                                                      type = "csv"),
            output_file = paste(data_path, "documentation/",
