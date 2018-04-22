3cb5e4b3b8f628e6ec2d76e2c4cef0de1810496f
stephdesilva <steph@rex-analytics.com>
2018-04-22 22:12:01 +1000

    Add basic documentation function.

---
+++ R/createBasicDocumentation.R
@@ -0,0 +1,15 @@
+createBasicDocumentation <- function(){
+
+  data_directory <- paste(getwd(), "/data", sep = "")
+  utils::download.file("https://raw.githubusercontent.com/stephdesilva/consultthat_scripts/master/basicDocumentation.Rmd",
+                       paste(getwd(), "/data/documentation/basicDocumentation.rmd", sep = ""))
+
+
+  files <- list.files(path = data_directory, recursive = FALSE)
+  for (i in 1:length(files)){
+      render(paste(data_directory,"/documentation/basicDocumentation.rmd", sep = ""),
+             params = list(nameFile = files[i],
+                           fileType = ".csv"),
+             output_file = paste("Basic Documentation", files[i], ".html", sep = ""))
+  }
+}
