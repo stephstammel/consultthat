3ff19fbac82647b05717d483d40f30d1ada0668d
stephdesilva <steph@rex-analytics.com>
2018-04-22 22:16:49 +1000

    Add namespaces to createBasicDocumentation()

--- R/createBasicDocumentation.R
+++ R/createBasicDocumentation.R
@@ -1,5 +1,6 @@
 createBasicDocumentation <- function(){
-
+  requireNamespace("knitr")
+  requireNamespace("rmarkdown")
   data_directory <- paste(getwd(), "/data", sep = "")
   utils::download.file("https://raw.githubusercontent.com/stephdesilva/consultthat_scripts/master/basicDocumentation.Rmd",
                        paste(getwd(), "/data/documentation/basicDocumentation.rmd", sep = ""))
@@ -7,7 +8,7 @@ createBasicDocumentation <- function(){
 
   files <- list.files(path = data_directory, recursive = FALSE)
   for (i in 1:length(files)){
-      render(paste(data_directory,"/documentation/basicDocumentation.rmd", sep = ""),
+      knitr::render(paste(data_directory,"/documentation/basicDocumentation.rmd", sep = ""),
              params = list(nameFile = files[i],
                            fileType = ".csv"),
              output_file = paste("Basic Documentation", files[i], ".html", sep = ""))
